82293e5a96137d8091935e92009c22b5394cfd7d
Belema Ogan <Oganbelema@gmail.com>
2019-05-11 20:19:34 +0100

    Add data binding to the project
    - Added cancel and enroll buttons
    - Added data binding handling for click events

--- app/src/main/java/com/anushka/androidtutz/bindingdemo1/MainActivity.java
+++ app/src/main/java/com/anushka/androidtutz/bindingdemo1/MainActivity.java
@@ -1,15 +1,20 @@
 package com.anushka.androidtutz.bindingdemo1;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.anushka.androidtutz.bindingdemo1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mActivityMainBinding;
+
+    private MainActivityClickHandlers mMainActivityClickHandlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
         mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mActivityMainBinding.setStudent(getCurrentStudent());
+
+        mMainActivityClickHandlers = new MainActivityClickHandlers(this);
+        mActivityMainBinding.setClickHandler(mMainActivityClickHandlers);
     }
 
     private Student getCurrentStudent(){
@@ -26,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
         student.setStudentName("Alex");
         student.setStudentEmail("alex@gmail.com");
         return student;
+
+    }
+
+    public class MainActivityClickHandlers {
+        final Context mContext;
+
+        public MainActivityClickHandlers(Context context) {
+            mContext = context;
+        }
+
+        public void onEnrollButtonClicked(View view){
+            Toast.makeText(mContext, "Enroll clicked!", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        public void onCancelButtonClicked(View view){
+            Toast.makeText(mContext, "Cancel clicked", Toast.LENGTH_LONG)
+                    .show();
+        }
+
 
     }
 }
